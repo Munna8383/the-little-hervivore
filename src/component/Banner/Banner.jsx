0f38724ffd5ade9ab7fac1c73c5f5aa42ef7aedc
Munna8383 <jahedmunna612@gmail.com>
2024-03-17 02:50:08 +0600

    fixed text size

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className="w-11/12 mx-auto mt-20 bg-[url(https://i.ibb.co/pR0ycjY/Group-5-1.png)] text-white px-2 py-6 lg:py-40 lg:px-10 bg-no-repeat bg-contain lg:bg-cover lg:space-y-8 space-y-1 rounded-xl overflow-hidden">
 
-           <div className="text-center text-2xl lg:text-6xl font-bold">
+           <div className="text-center text-2xl space-y-2 lg:text-5xl font-bold">
             <h1>Discovered An exceptional Cooking </h1>
             <h2>Class Tailored For You</h2>
            </div>
